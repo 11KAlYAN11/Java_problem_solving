cd6cfcef0e27fac9fb6cbe0307f54804778039f2
kalyan <programmer143143@gmail.com>
2025-01-17 15:42:49 +0000

    added Interface over Inheritance class

---
+++ OOPS/InterfaceOverInheritance.java
@@ -0,0 +1,86 @@
+package OOPS;
+
+/**
+ * BankAccount interface defines the methods for bank account operations.
+ */
+interface BankAccount {
+    void deposit(double amount);
+    void withdraw(double amount);
+}
+
+/**
+ * ICICIBank class implements the BankAccount interface.
+ * It provides specific implementations for deposit and withdraw methods.
+ */
+class ICICIBank implements BankAccount {
+    @Override
+    public void deposit(double amount) {
+        System.out.println("Depositing: " + amount + " to ICIC bank");
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        System.out.println("Withdrawing " + amount + " from ICIC Bank");
+    }
+}
+
+/**
+ * HDFCBank class implements the BankAccount interface.
+ * It provides specific implementations for deposit and withdraw methods.
+ */
+class HDFCBank implements BankAccount {
+    @Override
+    public void deposit(double amount) {
+        System.out.println("Depositing: " + amount + " to HDFC bank");
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        System.out.println("Withdrawing " + amount + " from HDFC Bank");
+    }
+}
+
+/**
+ * HCLEmployee class represents an employee with a bank account.
+ * It allows depositing and withdrawing salary through the associated bank account.
+ */
+class HCLEmployee {
+    String name;
+    BankAccount bankAccount;
+
+
+    HCLEmployee(String name, BankAccount bankAccount) {
+        this.name = name;
+        this.bankAccount = bankAccount;
+    }
+    // methods to add or deduct the salary
+    void depositSalary(double amount) {
+        bankAccount.deposit(amount);
+    }
+
+    void withdrawSalary(double amount) {
+        bankAccount.withdraw(amount);
+    }
+}
+
+/**
+ * InterfaceOverInheritance class contains the main method to demonstrate the functionality.
+ */
+public class InterfaceOverInheritance {
+    public static void main(String[] args) {
+        // Employee initially has an ICIC bank account
+        HCLEmployee emp1 = new HCLEmployee("pavan", new ICICIBank());
+        emp1.depositSalary(10000);
+        emp1.withdrawSalary(5000);
+
+        // Employee moves to JP Morgan (where only HDFC exists)
+        emp1.bankAccount = new HDFCBank();
+        emp1.depositSalary(20000);
+        emp1.withdrawSalary(8000);
+
+        // New employee joins with an HDFC account to HCL
+        HCLEmployee emp2 = new HCLEmployee("Preethi", new HDFCBank());
+        emp2.depositSalary(10000);
+        emp2.withdrawSalary(5000);
+    }
+}
--- OOPS/InterfaceSingleClassDemo.java
+++ OOPS/InterfaceSingleClassDemo.java
@@ -1,18 +1,27 @@
 package OOPS;
+
+/**
+ * Tree is an abstract class that represents a tree with a name.
+ * It contains an abstract method display() that must be implemented by subclasses.
+ */
 abstract class Tree {
-    //here Tree is a abstract class cuz we don't need any object instantation on Tree (bcz there was no any implementaion in Tree class)
     String name;
+
     Tree(String name) {
         this.name = name;
-        System.out.println(name+" tree class constructor");
+        System.out.println(name + " tree class constructor");
     }
-    // we are definig one abstract method (the child classes will implement their own implementation)
+
+    /**
+     * Abstract method that must be implemented by subclasses to display tree information.
+     */
     abstract void display();
 }
 
+/**
+ * Neem class extends the Tree class and provides an implementation for the display() method.
+ */
 class Neem extends Tree {
-    // Here we are passing arguments for Neem while object creation. that will pass
-    // to parent Tree class constructor
     Neem(String name) {
         super(name);
     }
@@ -22,24 +31,31 @@ class Neem extends Tree {
         System.out.println(name + " is a neem tree display method");
     }
 }
+
+/**
+ * Bamboo class extends the Tree class and provides an implementation for the display() method.
+ */
 class Bamboo extends Tree {
-    // Here we are passing arguments for Bamboo while object creation. that will pass
-    // to parent Tree class constructor
     Bamboo(String name) {
         super(name);
     }
+
     @Override
     void display() {
         System.out.println(name + " is a bamboo tree display method");
     }
 }
+
+/**
+ * InterfaceSingleClassDemo class contains the main method to demonstrate the creation of tree objects.
+ */
 public class InterfaceSingleClassDemo {
     public static void main(String[] args) {
         // Creating an object of the class
-       // Tree tree = new Tree("Tree"); // will give compile error bcz Tree is an abstract class
-       Tree neem = new Neem("Neem From AP");
-       Tree bamboo = new Bamboo("Bamboo from TG");
-       neem.display();
-       bamboo.display();
+        // Tree tree = new Tree("Tree"); // will give compile error because Tree is an abstract class
+        Tree neem = new Neem("Neem From AP");
+        Tree bamboo = new Bamboo("Bamboo from TG");
+        neem.display();
+        bamboo.display();
     }
 }
