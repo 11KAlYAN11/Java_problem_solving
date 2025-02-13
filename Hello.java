799d9fc98ad137fcdf9bb461be85c48e5dda4cf3
kalyan <programmer143143@gmail.com>
2025-02-13 15:43:46 +0000

    NA

--- Hello.java
+++ Hello.java
@@ -1,12 +1,34 @@
 public class Hello {
-    public static void loop(int n) {
-        for(int k=0; k<=n; k++) {
-            System.out.println(k);
-        }
-    }
+    
+   
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        loop(10);
+        System.out.println(isEven(101));
+        System.out.println(isEven1(100));
+
     }
+    public static boolean isEven(int num) {
+        return (num & 1) == 0;
+        /*
+         *   1010   (10 in binary)
+            & 0001   (1 in binary)
+            ------------
+            0000   (Result = 0)
+
+            1001   (9 in binary)
+            & 0001   (1 in binary)
+            ------------
+            0001   (Result = 1)
+         */
+    }
+    public static boolean isEven1(int num) {
+        int absN = Math.abs(num);
+        boolean out = true;
+        for(int i=0; i<absN; i++) {
+            out = !out; // keep on fliping from true to false, false to true
+        }
+        return out;
+    }
+
     
 }
