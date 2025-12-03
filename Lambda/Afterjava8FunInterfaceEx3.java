ea9e4ebdc3afb780307e79921fe54ce8157155fa
kalyan <programmer143143@gmail.com>
2025-12-03 17:21:20 +0000

    ex3 optimazation FunInterface

--- Lambda/Afterjava8FunInterfaceEx3.java
+++ Lambda/Afterjava8FunInterfaceEx3.java
@@ -5,8 +5,19 @@ public class Afterjava8FunInterfaceEx3 {
     public static Multiply helperFuncation() {
         return (x,y) -> x*y;
     }
-    public static void main(String[] args) {
+   /*  public static void main(String[] args) {
         Multiply m = helperFuncation();
         System.out.println(m.multiply(10, 20));
     }
+ */
+    // optimazation1: 
+    /* public static void main(String[] args) {
+        Multiply m = (x,y) -> x*y;
+        System.out.println(m.multiply(10, 20)); // Helper funcation is optional
+    } */
+
+        // Optimazation 2 no varible only
+        public static void main(String[] args) {
+             System.out.println(((Multiply)(x, y) -> x * y).multiply(10, 20));
+        }
 }
