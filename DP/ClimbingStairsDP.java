0e4969e5020bd95b3085f2e07496d0ccae500b25
kalyan <programmer143143@gmail.com>
2025-12-05 15:16:45 +0000

    DP practice examples2

---
+++ DP/ClimbingStairsDP.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+public class ClimbingStairsDP {
+    // will take 2 static arrays to access accross class
+     private static int[] res;
+     private static int[] res2; 
+    public static void main(String[] args) {
+
+        // 1) Top -Down memoization // 1st way ⭐⭐ Recursion + Memoization (Top-down DP)
+        int ans = climbStairs(6);
+        System.out.println("Ans: "+ans);
+        System.out.println("Res1: "+Arrays.toString(res));
+
+        // 2)  // ⭐⭐⭐ 3) Iterative DP / Tabulation (Bottom-up DP)
+        int ans2 = goClimb(10);
+        System.out.println("Ans: "+ans2);
+        System.out.println("Res2: "+Arrays.toString(res2));
+    }
+    public static int climbStairs(int n) {
+        res = new int[n+1];
+        Arrays.fill(res, -1);
+        return climb(n, res);
+    }
+
+    public static int climb(int n, int[] res) {
+        // base case
+        if(n<=2) return n;
+
+        if(res[n] != -1) return res[n]; // reuse the same result
+
+        res[n] = climb(n-1, res) + climb(n-2, res);
+
+        return res[n];
+    }
+    public static int goClimb(int n) {
+        res2 = new int[n+1];
+        Arrays.fill(res, -1);
+        res2[0] = 1;
+        res2[1] = 1;
+
+        for(int i=2; i<=n; i++) {
+            res2[i] = res2[i-1] + res2[i-2];
+        }
+        return res2[n];
+    }
+}
--- DP/Fibno1.java
+++ DP/Fibno1.java
@@ -1,14 +1,29 @@
 import java.util.Arrays;
 
 public class Fibno1 {
-    public static void main(String[] args) {
-        long res = fibMemo(5);
-        System.out.println("Fibonacci result: " + res);
-        System.out.println("Memoization array: " + Arrays.toString(memoArray));
-    }
 
     // Declare memoArray as a static variable to access it in main
     private static long[] memoArray;
+
+    private static int[] dp;
+
+    public static void main(String[] args) {
+        long num = fibMemo(5);
+
+        // 1st way ⭐⭐ Recursion + Memoization (Top-down DP)
+        System.out.println("1st way ⭐⭐ Recursion + Memoization (Top-down DP)");
+        System.out.println("Fibonacci result: " + num);
+        System.out.println("Memoization array: " + Arrays.toString(memoArray));
+
+        // ⭐⭐⭐ 3) Iterative DP / Tabulation (Bottom-up DP)
+        System.out.println("⭐⭐⭐ 3) Iterative DP / Tabulation (Bottom-up DP)");
+        int res2 = fibDP(5);
+        System.out.println("Fibonacci rsult2: "+ res2);
+        System.out.println("Array: "+Arrays.toString(dp));
+
+    }
+
+  
 
     public static long fibMemo(int n) {
         // 1) Create a memo array of size n+1 to store results for fib(0)..fib(n).
@@ -33,4 +48,44 @@ public class Fibno1 {
         memo[n] = fibMemoHelper(n - 1, memo) + fibMemoHelper(n - 2, memo);
         return memo[n];
     }
+
+    public static int fibDP(int n) {
+        dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+
 }
+
+
+/*
+fibMemoHelper(5)               // memo: [-1,-1,-1,-1,-1,-1]
+  -> needs fib(4) and fib(3)
+  fibMemoHelper(4)             // memo: [-1,-1,-1,-1,-1,-1]
+    -> needs fib(3) and fib(2)
+    fibMemoHelper(3)           // memo: [-1,-1,-1,-1,-1,-1]
+      -> needs fib(2) and fib(1)
+      fibMemoHelper(2)         // memo: [-1,-1,-1,-1,-1,-1]
+        -> needs fib(1) and fib(0)
+        fibMemoHelper(1) -> returns 1  // base case
+        fibMemoHelper(0) -> returns 0  // base case
+        // now memo[2] = 1 + 0 = 1
+        memo becomes: [-1,-1,1,-1,-1,-1]
+      // back to fib(3): fib(2) is now in memo -> returned instantly (1)
+      fibMemoHelper(1) -> returns 1
+      // memo[3] = fib(2) + fib(1) = 1 + 1 = 2
+      memo becomes: [-1,-1,1,2,-1,-1]
+    // back to fib(4): need fib(3) (found in memo -> 2) and fib(2) (found in memo -> 1)
+    // memo[4] = 2 + 1 = 3
+    memo becomes: [-1,-1,1,2,3,-1]
+  // back to fib(5): need fib(4) (3) and fib(3) (2)
+  // memo[5] = 3 + 2 = 5
+  memo becomes: [-1,-1,1,2,3,5]
+
+ */
