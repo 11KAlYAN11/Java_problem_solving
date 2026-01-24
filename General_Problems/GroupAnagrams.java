e23605f000dab9c892e976be13979d19bcdeb414
kalyan <programmer143143@gmail.com>
2026-01-24 16:11:45 +0000

    Add GroupAnagrams class with method to group anagrams from an array of strings

---
+++ General_Problems/GroupAnagrams.java
@@ -0,0 +1,57 @@
+package General_Problems;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class GroupAnagrams { // GroupAnagrams
+    public static ArrayList<ArrayList<String>> anagrams(String[] arr) {
+        // code here
+        ArrayList<ArrayList<String>> res = new ArrayList<>();
+        
+        HashMap<String, Integer> map = new HashMap<>(); // bcz in map will store hash: count 
+        
+        // this map1 is for 2nd method
+        HashMap<String, ArrayList<String>> map1 = new HashMap<>();
+        
+        /*for(int i=0; i<arr.length; i++) {
+            
+            String hash = getHash(arr[i]);
+            
+            if(!map.containsKey(hash)) { // If our map is not having the key add it
+                map.put(hash, map.size()); // adding that map.size() it bit tricky bit guessable
+                res.add(new ArrayList<>());
+            }
+            res.get(map.get(hash)).add(arr[i]);
+        }
+        return res;*/
+        for(String s: arr) {
+            String hash = getHash(s);
+            
+            map1.putIfAbsent(hash, new ArrayList<>());
+            map1.get(hash).add(s);
+        }
+        return new ArrayList<>(map1.values());
+    }
+    
+    // helper method
+    public static String getHash(String s) {
+        StringBuilder sb = new StringBuilder();
+        int[] freq = new int[26];
+        
+        for(char c: s.toCharArray()) freq[c - 'a']++; 
+        
+        for(int i=0; i<freq.length; i++) {
+            sb.append(freq[i]);
+            sb.append("$"); // delimeter is must if frequency of any cahrs go beyond single digit 
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String[] arr = {"cat", "dog", "tac", "god", "act"};
+        ArrayList<ArrayList<String>> res = anagrams(arr);
+        for(ArrayList<String> s: res) {
+            System.out.println(s.toString());
+        }
+    }
+}
