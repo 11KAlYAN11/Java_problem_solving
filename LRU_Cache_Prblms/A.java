16c9bc34eb19f143994e2cf40204ccb6d27e251e
Asam Pavan Kumar Reddy <76771308+11KAlYAN11@users.noreply.github.com>
2026-09-03 17:39:34 +0000

    startign with LRU in depth

---
+++ LRU_Cache_Prblms/A.java
@@ -0,0 +1,23 @@
+package LRU_Cache_Prblms;
+
+public class A {
+    /*
+        Capacity = 3
+
+        PUT A
+        PUT B
+        PUT C
+
+        Cache:
+        A  B  C
+
+        GET A       ← A becomes recently used
+
+        PUT D       ← cache full
+
+        Who leaves?
+        B           ← least recently used
+
+        So the cache needs to maintain recency order.
+     */
+}
